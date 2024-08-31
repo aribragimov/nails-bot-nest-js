53b50ba8e1277f80b809b98d02f3cfbaf3e78293
Artem Ibragimov <aribragimov17@gmail.com>
2024-08-31 12:07:54 +0300

    feat: add app module

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { DataSourceOptions } from 'typeorm';
+
+import { HealthController } from './health/health.controller';
+import { TelegramBotModule } from './module/tg-bot';
+import { SvcConfigModule } from './config';
+
+@Module({
+  imports: [
+    SvcConfigModule,
+    HttpModule.register({
+      timeout: 5000,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<{ database: DataSourceOptions }, true>) =>
+        configService.get('database'),
+    }),
+    TelegramBotModule,
+  ],
+  controllers: [HealthController],
+})
+export class AppModule {}
